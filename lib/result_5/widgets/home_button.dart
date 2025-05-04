92096d46a308bfa9131cf06a84a023350a118f23
oeoott <asdlatnwjd@gmail.com>
2025-05-05 03:34:47 +0900

    result 2,3,4,5

---
+++ lib/result_5/widgets/home_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../../../intro/intro_screen.dart';
+
+ // 홈 화면 연결 경로
+
+class HomeButton extends StatelessWidget {
+  const HomeButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // ✅ 홈 화면으로 이동
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const IntroScreen()),
+        );
+      },
+      child: Image.asset(
+        'assets/icons/home_icon.png', // ✅ 홈 아이콘 이미지 경로
+        width: 51,
+        height: 51,
+      ),
+    );
+  }
+}
